6e781b0998e5fc78148dbda62d70fad0e832b51f
Hermenegildo Marin Júnior <programador.marin@gmail.com>
2012-01-30 20:44:28 -0200

    index e bootstrap alterados

--- bootstrap.php
+++ bootstrap.php
@@ -1,7 +1,10 @@
 <?php
-set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__ . '/src');
-
 require_once 'Lcobucci/Common/ClassLoader/SplClassLoader.php';
+
+use Mindplay\Annotation\Cache\ApcCache;
+use Mindplay\Annotation\Core\Annotations;
 
 $loader = new Lcobucci\Common\ClassLoader\SplClassLoader();
 $loader->register();
+
+Annotations::$config['cache'] = new ApcCache();
--- public/index.php
+++ public/index.php
@@ -1,1 +1,17 @@
 <?php
+set_include_path(
+        get_include_path() . PATH_SEPARATOR .
+        realpath(__DIR__ . '/../src/') . PATH_SEPARATOR .
+        realpath(__DIR__ . '/../templates/')
+);
+
+require_once '../bootstrap.php';
+
+use \Lcobucci\ActionMapper\Core\ApplicationFactory;
+use \Lcobucci\Common\DependencyInjection\XmlContainerBuilder ;
+
+$app = ApplicationFactory::initApplication();
+$app->getSession()->start('StartDDD');
+$app->setDependencyContainer(XmlContainerBuilder::build(__DIR__ . '/../config/dic.xml'));
+
+$app->run();
